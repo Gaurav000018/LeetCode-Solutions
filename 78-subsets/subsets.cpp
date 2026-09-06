bd639e2d4aff8a1a1b5ff1ea31f7231285ba16ac
Gaurav Kumar Singh <gs9692412@gmail.com>
2026-09-06 16:04:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (59.50%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     void backtrack(int index,vector<int>&arr,vector<int>&ds,vector<vector<int>>&ans){
         if(index==arr.size()){
-            if(index==arr.size()){
             ans.push_back(ds);
         }
+        if(index==arr.size()){
             return;
         }
-        
         ds.push_back(arr[index]);
         backtrack(index+1,arr,ds,ans);
         ds.pop_back();
